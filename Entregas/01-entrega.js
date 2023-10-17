63fa55fd30ac1ea63c0e7076d0a420d2b850b42f
Gonzalo Catrilef <catrilef.gonzalo@icloud.com>
2023-10-17 01:16:51 -0300

    Creacion de addProduct

--- Entregas/01-entrega.js
+++ Entregas/01-entrega.js
@@ -1,21 +1,37 @@
 
 class ProductManger {
     constructor (){
-        this.products=[{code:4}]
+        this.products=[]
     }
 
     getProduct(){   
         let prod = this.products
-        //return console.log(prod)
+        return console.log(prod) 
     }
 
-    /* addProduct(nombre, description, price, thumbnail, code, stock){
+    addProduct(nombre, description, price, thumbnail, stock){
 
-        let code= 1
-        if(code)
+        let code = 1
 
-        
-    } */
+        if (this.products.length > 0) {
+            code = this.products[this.products.length-1].code + 1 
+        }else {
+            console.log(code)
+            console.error('Producto & Code ya existen',)
+        }
+
+        let newProduct = {
+            code,
+            nombre,
+            description,
+            price,
+            thumbnail,
+            stock
+        }
+
+
+        this.products.push(newProduct)
+    }
 
 
     getProductById(code){
@@ -29,6 +45,13 @@ class ProductManger {
 
 const productManager = new ProductManger()
 
-productManager.getProductById(4)
+
+productManager.addProduct('edam','Griego',7990,'img',10)
+productManager.addProduct('stilton','Ingles',5990,'img',8)
+productManager.addProduct('Feta','Turco',5990,'img',5)
+productManager.getProductById(3)
+
 productManager.getProduct()
+
+
 
