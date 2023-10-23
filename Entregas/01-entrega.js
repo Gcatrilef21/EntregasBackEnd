a0f4f3d7dbdf0e477e789c8d220f3e6039012d53
Gonzalo Catrilef <catrilef.gonzalo@icloud.com>
2023-10-23 20:01:36 -0300

    Correcion Primera entrega

--- Entregas/01-entrega.js
+++ Entregas/01-entrega.js
@@ -4,53 +4,68 @@ class ProductManger {
         this.products=[]
     }
 
-    getProduct(){   
-        let prod = this.products
-        return console.log(prod) 
+
+/* --> El método getProducts funciona correctamente, devuelve todos.✅ */
+
+    getProduct(){    
+        let product = this.products
+        return console.log(product)
     }
+    
+/* --> AddProduct: 
+    Se debe validar que no exista un producto con el mismo code. ✅
+    El code no es el mismo campo que id, el id debe ser autoincrementable y el code es otro campo distinto. ✅
+    Falta validar que todos los campos sean obligatorios. ✅ */
 
-    addProduct(nombre, description, price, thumbnail, stock){
+    addProduct(title, description, price, code, thumbnail, stock){
 
-        let code = 1
-
-        if (this.products.length > 0) {
-            code = this.products[this.products.length-1].code + 1 
-        }else {
-            console.error('Producto & Code ya existen',)
+        if(!title || !description || ! price || !code || !thumbnail || !stock){
+            console.log('All fields are required ')
+        }else{
+            let id = 1
+        if(this.products.length>0){
+            id=this.products[this.products.length-1].id + 1
         }
 
         let newProduct = {
-            code,
-            nombre,
+            id, 
+            title, 
             description,
-            price,
+            price, 
+            code,
             thumbnail,
             stock
         }
 
+        this.products.some(product => product.code === newProduct.code)
+            ? console.error(`This Product already exists name: ${newProduct.title} / code: ${newProduct.code}`)
+            : this.products.push(newProduct)
 
-        this.products.push(newProduct)
+        }
+    
+        
     }
 
+/*     -->  getProductById: 
+    Se debe buscar por id no por code. ✅
+    En caso de que exista se tiene que devolver el producto completo, no un mensaje de que si existe.  */ 
 
-    getProductById(code){
-        const idProduct = this.products.find(product => product.code === code)
-        //console.log(idProduct)
+    getProductById(id){
+        let idProduct = this.products.find(product => product.id === id)
         return idProduct 
-            ? console.log(`Este es el ID del Producto ${idProduct.code}`) 
-            : console.error('Not Found ❌')
+            ? console.log(idProduct) 
+            : console.error('Not Found ❌') 
     }
 }
 
-const productManager = new ProductManger()
+let productManager = new ProductManger()
 
 
-productManager.addProduct('edam','Griego',7990,'img',10)
-productManager.addProduct('stilton','Ingles',5990,'img',8)
-productManager.addProduct('Feta','Turco',5990,'img',5)
-productManager.getProductById(3)
+productManager.addProduct('Feta','Turco',200,12,'img',10)
 
+productManager.addProduct('stilton','Ingles',5990,1,'img',8)
+
+productManager.addProduct('Feta','Turco',5990,53,'img',5)
+//productManager.addProduct()
 productManager.getProduct()
-
-
-
+productManager.getProductById(5)
